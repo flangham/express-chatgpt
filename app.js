bd2310267532bb95481e168251099da7a34b909a
flangham <finnian.langham@gmail.com>
2023-12-19 17:02:09 +1100

    selective CORS origin

--- app.js
+++ app.js
@@ -8,29 +8,29 @@ const port = 8000;
 const openai = new OpenAI(process.env.OPENAI_API_KEY);
 
 // Middleware
-app.use(cors()); // This allows all origins
+// app.use(cors()); // This allows all origins
 
-// const allowedOrigins = [
-//   'http://localhost:8000',
-//   'https://loveandmoney.brandtoolk.it/',
-// ];
+const allowedOrigins = [
+  'http://localhost:8000',
+  'https://loveandmoney.brandtoolk.it/',
+];
 
-// app.use(
-//   cors({
-//     origin: function (origin, callback) {
-//       // Allow requests with no origin (like mobile apps or curl requests)
-//       if (!origin) return callback(null, true);
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
 
-//       if (!allowedOrigins.includes(origin)) {
-//         const msg =
-//           'The CORS policy for this site does not ' +
-//           'allow access from the specified Origin.';
-//         return callback(new Error(msg), false);
-//       }
-//       return callback(null, true);
-//     },
-//   })
-// );
+      if (!allowedOrigins.includes(origin)) {
+        const msg =
+          'The CORS policy for this site does not ' +
+          'allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 
 app.use(express.json());
 
